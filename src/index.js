ce6a221a2bdf0fdf0b86133386b31ad2b3edfcf8
Mateusz Widerski <mat.widerski@gmail.com>
2018-06-21 19:24:22 +0200

    Add reducer.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import reducer from './reducer.js';
 
 const store = createStore(reducer);
 

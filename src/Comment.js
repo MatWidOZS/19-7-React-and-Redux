e5bbee7428d064f8389b8e8cbe0ea1172fb4f315
Mateusz Widerski <mat.widerski@gmail.com>
2018-06-25 11:32:55 +0200

    Add thumb down, edit comment, remove comment actions

--- src/Comment.js
+++ src/Comment.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
-const Comment = ({text, votes, id, thumbUpComment}) => 
+const Comment = ({text, votes, id, thumbUpComment, thumbDownComment, editComment, removeComment}) => 
 	<li>
-		{text} <span>votes: {votes}</span> <button onClick={() => thumbUpComment(id)}>Thumb up</button>
+		{text} <span>votes: {votes}</span>
+		<button onClick={() => thumbUpComment(id)}>Thumb up</button>
+		<button onClick={() => thumbDownComment(id)}>Thumb down</button>
+		<button onClick={() => editComment(id, prompt(text))}>Edit</button>
+		<button onClick={() => removeComment(id)}>X</button>
 	</li>;
 
 export default Comment;
